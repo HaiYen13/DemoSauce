3e1a7417da4611d049028154f03d62665e27574c
HaiYen13 <haiyencute13@gmail.com>
2025-08-31 23:56:40 +0700

    testAPI

---
+++ tests/testAPI.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test';
+
+test('Test API', async({request}) => {
+    const response = await request.get('https:/restful-booker.herokuapp.com/booking');
+        console.log(await response.json())
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200)
+})
